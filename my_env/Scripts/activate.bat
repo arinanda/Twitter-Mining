bff27de531b6ddbb79b0519027c8111aaf2de4b9
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2018-09-12 10:17:02 +0700

    add gitignore

--- my_env/Scripts/activate.bat
+++ my_env/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=D:\TUGAS AKHIR\Twitter-Mining\my_env"
+set "VIRTUAL_ENV=E:\Project\Twitter-Mining\my_env"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
